d85c608493ab6a1522187b3ceb12d4786232b80d
Lagan Mehta <laganmehta.in@gmail.com>
2025-07-17 11:37:10 +0530

    Server Set Up

---
+++ Server/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+const port = 3000;
+app.use(cors());
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  const userName = "LAGAN mEHTA";
+  res.render('template.ejs', { name: userName });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
 
 import './App.css'
-import {Routes,Route} from "react-router-dom";
-import Dashboard from "../src/Dashboard/index.jsx";
-import Portfolio from "../src/Portfolio/index.jsx";
-import Login from "../src/Login/index.jsx";
-import Signup from "../src/Signup/index.jsx";
+import { useEffect,useState } from 'react';
+// import {Routes,Route} from "react-router-dom";
+// import Dashboard from "../src/Dashboard/index.jsx";
+// import Portfolio from "../src/Portfolio/index.jsx";
+// import Login from "../src/Login/index.jsx";
+// import Signup from "../src/Signup/index.jsx";
 function App() {
+  const [html, setHtml] = useState('');
 
+  useEffect(() => {
+    fetch('http://localhost:3000')
+      .then(res => res.text())
+      .then(data => setHtml(data));
+  }, []);
 
   return (
+
     <>
-      <Routes>
+      <div>
+      <h1>This is React Section</h1>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+    </div>
+      {/* <Routes>
           <Route path='/Login/:type'  element={<Login/>}/>
            <Route path='/Signup'  element={<Signup/>}/>
           <Route path='/Portfolio'  element={<Portfolio/>}/>
           <Route path='/Dashboard'  element={<Dashboard/>}/>
-      </Routes>
+      </Routes> */}
     </>
   )
 }
 
-export default App
+export default App;
